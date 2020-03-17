7dcf2ad6b97a28add037b3ee9870c58babf79825
numankh <khannuman15@gmail.com>
2020-03-16 20:34:39 -0400

    fixed formatting for FeedItem component

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
-const FeedItem = () => (
-    <Paper >
-        <h1>Article title</h1>
-        <p>Article description</p>
-    </Paper>
-)
+const useStyles = makeStyles(theme => ({
+    paper: {
+        marginTop: '15px',
+        width: '100%',
+        padding: '5px',
+        paddingLeft: '2%',
+        maxWidth: '800px'
+    }
+}));
 
-export default FeedItem
+
+export default function MiniDrawer() {
+    const classes = useStyles();
+
+    return (
+        <Paper className={classes.paper}>
+            <h2>Article title and source</h2>
+            <p>Article description</p>
+        </Paper>
+    )
+}
