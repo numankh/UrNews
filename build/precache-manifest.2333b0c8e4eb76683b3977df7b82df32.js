fb0045016f8a9d78501fade057bb3c89199b9820
numankh <khannuman15@gmail.com>
2020-06-02 16:43:27 -0400

    added server.js

---
+++ build/precache-manifest.2333b0c8e4eb76683b3977df7b82df32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1c4e1c4de1b2b3caa012d1903dcf49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab8112d17ae427012b0e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ff8afd82e270c2bc250b",
+    "url": "/static/js/2.c403f9e2.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.c403f9e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8112d17ae427012b0e",
+    "url": "/static/js/main.979aca35.chunk.js"
+  },
+  {
+    "revision": "cd057a1c5f7a46b132a8",
+    "url": "/static/js/runtime-main.f6a02578.js"
+  }
+]);
